a37cebc1686076348b26b93eb73928b8a15a8007
tomtomgo92 <tmillet@thatmuch.fr>
2026-02-28 18:31:56 +0100

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/', // Base URL for custom domain or user/org site
+    base: '/markUp/', // Correct base for GitHub Pages in sub-repository
 })
